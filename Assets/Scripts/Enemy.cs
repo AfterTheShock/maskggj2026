3e8df3f2a445f1e58d5f53de9e6fe448615152f2
Juan Ignacio Garcia <juanigarciadev@gmail.com>
2026-01-31 12:57:21 -0300

    Se crea el script de enemigo y la escena de Juani

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Referencias")]
+    [SerializeField] private Transform player;
+
+    [Header("Configuración")]
+    public float enemyHealth = 100f;
+    public float enemyDamage = 10f;
+    public float attackCooldown = 2.0f;
+    public float attackRange = 2.0f;
+
+    [Header("Estado")]
+    public bool enemyIsAtacking;
+    private float lastAttackTime;
+
+    private void Update()
+    {
+        // Mide la distancia entre el enemigo (transform.position) y la posición del jugador
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if(distance <= attackRange && Time.time >= lastAttackTime + attackCooldown && !enemyIsAtacking)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        enemyIsAtacking = true;
+        // Guarda el momento de ataque
+        lastAttackTime = Time.time;
+        Debug.Log("El enemigo comenzo a atacar");
+
+        // Se ejecuta en una corrutina para evitar el bloqueo del motor (ejecucion en paralelo)
+        StartCoroutine(HitLogic());
+    }
+
+    System.Collections.IEnumerator HitLogic()
+    {
+        //player.GetComponent<Health>().TakeDamage(enemyDamage);
+        Debug.Log("Golpe realizado");
+        
+        // Tiempo de espera hasta permitir que el enemigo ataque de nuevo
+        yield return new WaitForSeconds(attackCooldown);
+        enemyIsAtacking = false;
+    }
+}
