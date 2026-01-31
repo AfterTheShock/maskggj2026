b1c452dcd37158e3676a4fd73e9cd61f67cf5746
Gabriel Badui <gjbadui@gmail.com>
2026-01-31 17:34:45 -0300

    reload & ui

--- Assets/Scripts/PlayerShootingManager.cs
+++ Assets/Scripts/PlayerShootingManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerShootingManager : MonoBehaviour
 {
@@ -13,17 +15,37 @@ public class PlayerShootingManager : MonoBehaviour
     [SerializeField] Vector3 shootRecoilRotation = new Vector3(350,0,0);
     [SerializeField] Vector3 shootRecoilRandomRotation = new Vector3(5,5,5);
     [SerializeField] float backToNormalLerpSpeed = 15;
+    
+    [Header("Gun Func")]
+    [SerializeField] private int magazineAmmo;
+    [SerializeField] private int totalAmmo;
+    [SerializeField] private float reloadCooldown;
+    private int currentMagazineAmmo;
+    private float currentReloadCooldown = 0;
+    private bool reloading;
+
+    private void Start()
+    {
+        currentMagazineAmmo = magazineAmmo;
+        UserInterfaceUpdate();
+    }
 
     void Update()
     {
         timeLeftToShoot -= Time.deltaTime;
+        
+        currentReloadCooldown -= Time.deltaTime;
+        if (currentReloadCooldown > 0) reloading = true;
+        else reloading = false;
 
+        if (reloading) UserInterfaceManager.Instance.ReloadingEffect(totalAmmo);
+        else UserInterfaceManager.Instance.NormalEffect();
+        
+        if (Input.GetKeyDown(KeyCode.R)) Reload();
+        
         if(timeLeftToShoot <= 0)
         {
-            if (Input.GetMouseButton(0)) 
-            {
-                Shoot();
-            } 
+            if (Input.GetMouseButton(0)) Shoot();
         }
 
         //gunHolder.localEulerAngles = Vector3.Lerp(gunHolder.localEulerAngles, new Vector3(359,0,0), Time.deltaTime * backToNormalLerpSpeed);
@@ -32,6 +54,9 @@ public class PlayerShootingManager : MonoBehaviour
 
     private void Shoot()
     {
+        if (reloading) return;
+        if(currentMagazineAmmo <= 0) return;
+        
         timeLeftToShoot = shootCooldown - shootCooldown * (UpgradeableStatsSingleton.Instance.fireRate - 1);
 
         Transform cameraTransform = Camera.main.transform;
@@ -46,6 +71,29 @@ public class PlayerShootingManager : MonoBehaviour
         }
 
         VisualRecoil();
+        
+        currentMagazineAmmo--;
+        
+        UserInterfaceUpdate();
+    }
+
+    private void Reload()
+    {
+        if (reloading) return;
+        if (magazineAmmo <= currentMagazineAmmo) return;
+        
+        int ammoDifference = magazineAmmo - currentMagazineAmmo;
+        currentMagazineAmmo = magazineAmmo;
+        totalAmmo -= ammoDifference;
+
+        currentReloadCooldown = reloadCooldown;
+        
+        UserInterfaceUpdate();
+    }
+
+    private void UserInterfaceUpdate()
+    {
+        UserInterfaceManager.Instance.UpdateAmmoText(currentMagazineAmmo, totalAmmo);
     }
 
     private void VisualRecoil()
---
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class UserInterfaceManager : MonoBehaviour
+{
+    #region SingletonPattern
+    private static UserInterfaceManager _instance;
+
+    public static UserInterfaceManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    #endregion
+    
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    public void UpdateAmmoText(int leftSideAmmo, int rightSideAmmo)
+    {
+        ammoText.text = $"{leftSideAmmo}/{rightSideAmmo}";
+    }
+
+    public void ReloadingEffect(int rightSideAmmo)
+    {
+        ammoText.color = Color.red;
+    }
+
+    public void NormalEffect()
+    {
+        ammoText.color = Color.white;
+    }
+}
